ae971a89e22222f1ae9a97377920324105676e50
JacksonFA <jackson.f205@gmail.com>
2024-08-21 20:47:12 -0300

    add .gitignore to variables

--- variables/target/debug/deps/variables-d8ed52ef1944a3b2.d
+++
@@ -1,5 +0,0 @@
-/Users/espresso_code/www/github/me/rust-studies/variables/target/debug/deps/libvariables-d8ed52ef1944a3b2.rmeta: src/main.rs
-
-/Users/espresso_code/www/github/me/rust-studies/variables/target/debug/deps/variables-d8ed52ef1944a3b2.d: src/main.rs
-
-src/main.rs:
